ab0c1cde2f546792f5e35f5f63783041a843c6d8
Jesse Showalter <iamjesseshowalter@gmail.com>
2014-09-12 23:47:28 -1000

    mouse up for rye and more
    added mousedown and mouse up features

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
-	var ready = $($('.ryu-ready'));
-	var still = $($('.ryu-still'));
+	var ryuReady = $($('.ryu-ready'));
+	var ryuStill = $($('.ryu-still'));
+	var ryuThrow = $($('.ryu-throwing'));
+	var hadouken = $($('.hadouken'));
 
 	$('.ryu').mouseenter(function(){
-		still.hide();
-		ready.show();
+		ryuStill.hide();
+		ryuReady.show();
 	})
 	.mouseleave(function(){
-		ready.hide();
-		still.show();
+		ryuReady.hide();
+		ryuStill.show();
+	})
+	.mousedown(function(){
+		// play hadouken sound
+		ryuReady.hide();
+		ryuThrow.show();
+		hadouken.show();
+    	// show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function(){
+		ryuThrow.hide();
+  		ryuReady.show();
 	});
 });
